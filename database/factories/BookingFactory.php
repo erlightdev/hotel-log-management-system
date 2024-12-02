25b91d51965c82e0fccbdcb155caa0c179837fd2
erlightdev <lightdeveloper@protonmail.com>
2024-12-02 12:57:51 +0545

    laravel pint ran

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -2,10 +2,9 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 use App\Models\Booking;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class BookingFactory extends Factory
 {
@@ -23,12 +22,12 @@ class BookingFactory extends Factory
     {
         return [
             'user_id' => User::factory(),
-            'room_type' => $this->faker->randomElement(["single","double","suite","family","deluxe"]),
+            'room_type' => $this->faker->randomElement(['single', 'double', 'suite', 'family', 'deluxe']),
             'check_in_date' => $this->faker->date(),
             'check_out_date' => $this->faker->date(),
             'total_guests' => $this->faker->numberBetween(-10000, 10000),
             'total_price' => $this->faker->word(),
-            'booking_status' => $this->faker->randomElement(["pending","confirmed","checked_in","checked_out","cancelled"]),
+            'booking_status' => $this->faker->randomElement(['pending', 'confirmed', 'checked_in', 'checked_out', 'cancelled']),
         ];
     }
 }
--- database/migrations/2024_12_02_123601_create_bookings_table.php
+++ database/migrations/2024_12_02_123601_create_bookings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class() extends Migration
 {
     /**
      * Run the migrations.
@@ -14,12 +14,12 @@ return new class extends Migration
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
-            $table->enum('room_type', ["single","double","suite","family","deluxe"]);
+            $table->enum('room_type', ['single', 'double', 'suite', 'family', 'deluxe']);
             $table->date('check_in_date');
             $table->date('check_out_date');
             $table->integer('total_guests');
             $table->decimal('total_price', 10, 2);
-            $table->enum('booking_status', ["pending","confirmed","checked_in","checked_out","cancelled"])->default('pending');
+            $table->enum('booking_status', ['pending', 'confirmed', 'checked_in', 'checked_out', 'cancelled'])->default('pending');
             $table->timestamps();
             $table->softDeletes();
         });
